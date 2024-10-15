0d65d89f5a4c6b9a12f08b591566c941b38083ec
thseodud <sonda13578@gmail.com>
2024-10-15 13:34:51 -0400

    Add factorial function

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 3# Put your function here
-
+def factorial(n):
+        x = int(n)
+        if (x < 1 ):
+                return "Invalid number"
+        if x == 1:
+                return n
+        elif (x > 1):
+                return x *factorial(x-1)
+        
 
 # testing
-num = 5
+num = int(input(""))
 print(factorial(num))
